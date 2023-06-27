5a58b8f40832c668eb6d196beda39fff4fa5fb6f
Chiemelie10 <chiemelieeze@yahoo.com>
2023-06-27 22:25:53 +0000

    Task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { createUser, uploadPhoto } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([Image, User]) => {
+      console.log(`${Image.body} ${User.firstName} ${User.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
