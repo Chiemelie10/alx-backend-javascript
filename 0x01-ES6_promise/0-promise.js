33293887e2245c6f7a91371c5ca48a9645b62328
Chiemelie10 <chiemelieeze@yahoo.com>
2023-06-27 18:12:51 +0000

    Task 0

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    const project = 'JavaScript Promise';
-
-    if (project) {
-      resolve('Learning JavaScript Promise');
-    } else {
-      reject(Error);
-    }
-  });
+  return Promise.resolve();
 }
