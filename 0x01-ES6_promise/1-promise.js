6cf080946adc6a2279affc068e15f02c98379192
Chiemelie10 <chiemelieeze@yahoo.com>
2023-06-27 20:11:54 +0000

    Task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,8 @@
-export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({
-        status: 200,
-        body: 'Success'
-      });
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
-  });
+export default function getResponseFromAPI(success) {
+  const resBody = {
+    status: 200,
+    body: 'Success',
+  };
+  const error = new Error('The fake API is not working currently');
+  return success ? Promise.resolve(resBody) : Promise.reject(error);
 }
