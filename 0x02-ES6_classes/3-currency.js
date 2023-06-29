f3e1b0182462df6cf4ee409f0848642215104e6a
Chiemelie10 <chiemelieeze@yahoo.com>
2023-06-29 17:16:44 +0000

    Task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,38 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._code = code; // eslint-disable-line no-underscore-dangle
+    this._name = name; // eslint-disable-line no-underscore-dangle
+  }
+
+  get code() {
+    return this._code; // eslint-disable-line no-underscore-dangle
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = value; // eslint-disable-line no-underscore-dangle
+  }
+
+  get name() {
+    return this._name; // eslint-disable-line no-underscore-dangle
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value; // eslint-disable-line no-underscore-dangle
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
